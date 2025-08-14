5fc4f1b5b5f60eb939f68e9b3cbc458d9fddd857
kk0420-ai <kk0420@srmist.edu.in>
2025-08-14 05:05:46 +0000

    String Length

---
+++ S1/labProblems/stringLength.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class stringLength {
+    
+    public static int findLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.next();
+        
+        int customLength = findLength(input);
+        int builtInLength = input.length();
+        
+        System.out.println("Custom length: " + customLength);
+        System.out.println("Built-in length: " + builtInLength);
+    }
+}
